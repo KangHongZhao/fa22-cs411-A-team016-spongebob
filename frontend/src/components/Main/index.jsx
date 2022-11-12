7f7b9bea85eba640374d4d8aef7626f1488aa2f8
KangHongZhao <kz8@illinois.edu>
2022-11-11 21:17:34 -0600

    add info page

--- frontend/src/components/Main/index.jsx
+++ frontend/src/components/Main/index.jsx
@@ -1,17 +1,68 @@
-import { Link } from "react-router-dom";
-const main = () => {
+import { useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+
+const Main = () => {
+	const [data, setData] = useState({
+		UserId: "",
+		NewPassword: "",
+	});
+	const [error, setError] = useState("");
+	const navigate = useNavigate();
+
+	const handleChange = ({ currentTarget: input }) => {
+		setData({ ...data, [input.name]: input.value });
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			const url = "http://localhost:80/signup";
+			const { data: res } = await axios.post(url, data);
+			navigate("/login");
+			console.log(res.message);
+		} catch (error) {
+			if (
+				error.response &&
+				error.response.status >= 400 &&
+				error.response.status <= 500
+			) {
+				setError(error.response.data.message);
+			}
+		}
+	};
+
 	return (
 		<div>
-			<nav>
-				<h1>Personal Information</h1>
+			<form onSubmit={handleSubmit}>
+			    <h1>Personal Information</h1>
+			    <input
+					type="UserId"
+					placeholder="UserId"
+					name="UserId"
+					onChange={handleChange}
+					value={data.UserId}
+				/>
+			    <input
+					type="NewPassword"
+					placeholder="NewPassword"
+					name="NewPassword"
+					onChange={handleChange}
+					value={data.NewPassword}
+				/>
+				<button type="submit">
+						reset password
+				</button>
+				<div>
 				<Link to="/search">
 				<button type = 'button'>
 					go to search
 				</button>
 				</Link>
-			</nav>
+				</div>
+			</form>
 		</div>
 	);
 };
 
-export default main;
+export default Main;
